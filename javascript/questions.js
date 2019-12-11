9ec2ea6000fa8a17c5c35e8fdee62fb25f0f13cf
M Grimes <matthewlgrimes@gamil.com>
2019-12-11 15:48:05 -0700

    Quiz now runs. Checks answer

--- javascript/questions.js
+++ javascript/questions.js
@@ -1,45 +1,84 @@
 //Dom elements
-var qHolder = $('#question-holder');
-var start = $('#start');
+var qHolderEl = $('#question-holder');
+var startEl = $('#start');
 var counterEl = $('#counterEl');
-var buttonSpace = $('#button-space');
+var navEl = $('#nav-btns');
+var scoreEl = $('#scoreEl');
 var counter = 60;
+var score = 0;
 
+var questions = [
+    {
+        title: "What does DOM stand for?",
+        choices: ["Document Oriented Model", "Dedicated Origami Maker","Document Object Model", "Document Organization Method"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "In Jquery, how do you add a click event",
+        choices: [".addEventListener()", ".click()", ".addClickEvent()",".listenFor('click')"],
+        answer: ".click()"
+    },
+    {
+        title: "In the array ['apples', 'oranges', 'grapes','melons'] What is at index[2]",
+        choices: ["apples", "oranges", "grapes", "melons"],
+        answer: "grapes"
+    }
+]
+var currentPage = 0;
+var totalPages = questions.length;
 
-start.click(function() {
-    startTimer();
+startEl.click(function() {
     startQuiz();
+})
+
+qHolderEl.on('click','button', function() {
+    var result = $('<p>');
+    if (($(event.target).attr('data-selection') === questions[currentPage].answer)) {
+        result.addClass('text-success');
+        result.text('Correct!');
+        qHolderEl.append(result);
+        setTimeout(function() {
+        score++
+        currentPage ++
+        renderQuestions();
+        },2000);
+    } if (($(event.target).attr('data-selection') !== questions[currentPage].answer)) {
+        result.addClass('text-danger');
+        result.text('Incorrect!');
+        qHolderEl.append(result);
+        setTimeout(function() {
+        currentPage ++
+        renderQuestions();
+        },2000);
+}
 });
 
-function startTimer () {
-    setInterval(function(){
-        if (counter > 0) {
-         counter--;
-         counterEl.text(`Time: ${counter}`);
-         console.log(counter);
-        } if (counter === 0) {
-            clearInterval(counter);
-        }
-    },1000); 
-}
-
 function startQuiz () {
-    start.detach();
-    addNextButton();
-    var nextBtn = $('#next');
-    nextBtn.click(nextQuestion);
+    startEl.addClass('hide');
+    renderQuestions();
+
 }
 
-function addNextButton () {
-    var newBtn = 
-    $('<button type="button" class="btn btn-lg btn-primary" id="next"><span>Next</span></button>');
-    newBtn.appendTo($('#btn-space'));
+function renderQuestions () {
+    qHolderEl.empty();
+    scoreEl.text(`Score: ${score}`);
+    if (currentPage >= totalPages) {
+        endQuiz();
+        return;
+    }
+    var createQuestion = $('<div class="title bg-warning pt-1 pb-1"></div>');
+    createQuestion.text(questions[currentPage].title);
+    qHolderEl.append(createQuestion);
+    for (var i = 0; i < questions[currentPage].choices.length; i++) {
+        var createAnswer = $('<button type="button" class="btn btn-block btn-primary"></button>');
+        createAnswer.text(`${questions[currentPage].choices[i]}`);
+        createAnswer.attr("data-selection", `${questions[currentPage].choices[i]}`);
+        qHolderEl.append(createAnswer);
+    }
 }
 
-function buildQuestions () {
-    
-}
-// needs to check if anything was clicked, and if the answer was correct
-function nextQuestion () {
-
+function endQuiz() {
+    var quizResults = $('<div class="text-center results"></div>');
+    quizResults.text(`Your Score is ${score}/${totalPages}`);
+    qHolderEl.append(quizResults);
 }
